ec16c998c7e7c0ac8857e8f9fcc838685e99d6d5
jtdimasaka <jtd33@cam.ac.uk>
2023-06-23 23:46:36 +0100

    Remove unused codes

--- code/modelLoss.m
+++
@@ -1,7 +0,0 @@
-function [loss,gradients] = modelLoss(parameters,X,A,T)
-
-    Y = model(parameters,X,A);
-    loss = crossentropy(Y,T,1./(sum(T,1)/sum(sum(T))/min(sum(T,1)/sum(sum(T)))),'WeightsFormat','C',DataFormat="BC");
-    gradients = dlgradient(loss, parameters);
-
-end
